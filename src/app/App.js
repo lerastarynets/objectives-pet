daa9fccfb66561b8b35540f62ba5387058ea4e45
Valeriia Starynets <lera@wizer-training.com>
2022-12-06 20:17:02 +0100

    create main page, update routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Header } from '../components/common/header';
+import { MainPage } from '../components/mainPage';
 import { RickAndMorty } from '../components/rickAndMorty';
 import './App.css';
 
@@ -9,6 +10,8 @@ const App = () => (
         <Header />
         <div className="pt-16">
             <Routes>
+                <Route path="/" element={<Navigate to="/main" replace />} />
+                <Route exact path="/main" element={<MainPage />} />
                 <Route
                     exact
                     path="/characters"
---
+++ src/components/mainPage/MainPage.js
@@ -0,0 +1,9 @@
+import RickAndMortyLogo from '../../assets/RickAndMortyLogo.png';
+
+const MainPage = () => (
+    <div className="flex flex-col justify-center items-center p-8">
+        <img src={RickAndMortyLogo} alt="RickAndMortyLogo" />
+    </div>
+);
+
+export default MainPage;
